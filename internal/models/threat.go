bd8457103fa2a6d6b7eab0cdb0e24aed61ac11af
DESOLATE17 <taktashovadasha@yandex.ru>
2023-12-27 15:52:45 +0300

    fixed button and db

--- internal/models/threat.go
+++ internal/models/threat.go
@@ -1,10 +1,11 @@
 package models
 
 type Threat struct {
-	ThreatId    int `gorm:"primaryKey"`
-	Name        string
-	Description string
-	Image       string
-	Count       int
-	Price       int
+	ThreatId    int    `gorm:"primaryKey" json:"threatId"`
+	Name        string `json:"name"`
+	Summary     string `json:"summary"`
+	Description string `json:"description"`
+	Image       string `json:"image"`
+	Count       int    `json:"count"`
+	Price       int    `json:"price"`
 }
